7900ece2860468cb034f8483d8b08ccd6d8eed44
deiby <thefrogzes@gmail.com>
2024-06-07 23:09:05 -0400

    cambio

--- prueba.py
+++ prueba.py
@@ -1,16 +1,13 @@
 a = int(input("Ingresa un numero: "))
 b = int(input("Ingresa otro numero: "))
 n = [a, b]
-
-while True:
-    
+while True:    
     mas_numeros = input("Quiere ingresar mas numeros? yes/no:\n")
     if mas_numeros == "yes":
         x = int(input("Ingresa otro numero: "))
         n.append(x)
     elif mas_numeros == "no":
         break
-
 cantidad_numeros = len(n)
 try:
     print ((a + b + x) / cantidad_numeros)
