c735be3d342b8e0b66b4728f9746c7ff4b2ac0fa
deiby <thefrogzes@gmail.com>
2024-06-07 22:46:33 -0400

    promedio de numeros

--- prueba.py
+++ prueba.py
@@ -1,2 +1,18 @@
-print ("Hola")
+a = int(input("Ingresa un numero: "))
+b = int(input("Ingresa otro numero: "))
+n = [a, b]
 
+while True:
+    
+    mas_numeros = input("Quiere ingresar mas numeros? yes/no:\n")
+    if mas_numeros == "yes":
+        x = int(input("Ingresa otro numero: "))
+        n.append(x)
+    elif mas_numeros == "no":
+        break
+
+cantidad_numeros = len(n)
+try:
+    print ((a + b + x) / cantidad_numeros)
+except:
+    print ((a + b) / cantidad_numeros)
